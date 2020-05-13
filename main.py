d10a3a39c91732dbae58b2c488fb1e95b8b00a90
Saurabh <saurabhbraryo@gmail.com>
2020-05-13 15:48:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import numpy as np
+import graph
+
+
+class Matrix:
+
+    def __init__(self, x=None, y=None, z=None):
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+        if z is not None:
+            self.z = z
+
+    def add(self, x, y, z=None):
+        self.__init__(x, y, z)
+        result = np.add(self.x, self.y)
+        return result
+
+    def sub(self, x, y, z=None):
+        self.__init__(x, y, z)
+        result = np.subtract(self.x, self.y)
+        return result
+
+    def mult(self, x, y):
+        self.__init__(x, y)
+        result = np.multiply(self.x, self.y)
+        return result
+
+    def det(self, x):
+        self.__init__(x)
+        result = np.linalg.det(self.x)
+        return "%.2f" % round(result, 2)
+
+matrix = Matrix()
+x1 = [[1, 2], [3, 0]]
+x2 = [[1, 3], [2, 3]]
+x3 = matrix.add(x1, x2)
+print(x3)
